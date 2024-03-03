496e794399d342aeb168e85b7b4a5b773b8fdcc4
danielzespitia <daniel.z.espitia@gmail.com>
2024-03-03 01:13:46 -0400

    d

--- week01/code/ArraysTester.cs
+++ week01/code/ArraysTester.cs
@@ -34,28 +34,40 @@ public static class ArraysTester {
     /// <returns>array of doubles that are the multiples of the supplied number</returns>
     private static double[] MultiplesOf(double number, int length)
     {
-        // TODO Problem 1 Start
-        // Remember: Using comments in your program, write down your process for solving this problem
-        // step by step before you write the code. The plan should be clear enough that it could
-        // be implemented by another person.
+        // Step 1: Create an array of doubles with the specified length
+        double[] multiples = new double[length];
 
-        return new double[0]; // replace this return statement with your own
+        // Step 2: Use a loop to calculate the multiples
+        for (int i = 0; i < length; i++)
+        {
+            multiples[i] = number * (i + 1);
+        }
+
+        // Step 3: Return the array of multiples
+        return multiples;
     }
-    
+
     /// <summary>
-    /// Rotate the 'data' to the right by the 'amount'.  For example, if the data is 
-    /// <c>&lt;List&gt;{1, 2, 3, 4, 5, 6, 7, 8, 9}</c> and an amount is 3 then the list returned should be 
-    /// <c>&lt;List&gt;{7, 8, 9, 1, 2, 3, 4, 5, 6}</c>.  The value of amount will be in the range of <c>1</c> and 
-    /// <c>data.Count</c>.
-    /// <br /><br />
-    /// Because a list is dynamic, this function will modify the existing <c>data</c> list rather than returning a new list.
+    /// Rotate the 'data' to the right by the 'amount'.  
+    /// For example, if the data is <List>{1, 2, 3, 4, 5, 6, 7, 8, 9} and an amount is 3, 
+    /// then the list returned should be <List>{7, 8, 9, 1, 2, 3, 4, 5, 6}.  
+    /// The value of amount will be in the range of 1 and data.Count.
     /// </summary>
     private static void RotateListRight(List<int> data, int amount)
     {
-        // TODO Problem 2 Start
-        // Remember: Using comments in your program, write down your process for solving this problem
-        // step by step before you write the code. The plan should be clear enough that it could
-        // be implemented by another person.
+        // Step 1: Use modulo to ensure the amount is within the range of 1 and data.Count
+        amount = amount % data.Count;
 
+        // Step 2: Create a temporary list to store the rotated elements
+        List<int> rotatedElements = new List<int>();
+
+        // Step 3: Add the elements that need to be rotated to the temporary list
+        rotatedElements.AddRange(data.GetRange(data.Count - amount, amount));
+
+        // Step 4: Remove the rotated elements from the original list
+        data.RemoveRange(data.Count - amount, amount);
+
+        // Step 5: Insert the rotated elements at the beginning of the original list
+        data.InsertRange(0, rotatedElements);
     }
 }
